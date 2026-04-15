e17fb17318fa21da8bfb856405311bbb3ad62857
alisson <arteaga10alisson@gmail.com>
2026-04-14 23:47:22 -0500

    first commit

---
+++ src/main/java/com/aarteaga/app_redis_springboot/AppRedisSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.aarteaga.app_redis_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppRedisSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppRedisSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aarteaga/app_redis_springboot/AppRedisSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aarteaga.app_redis_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppRedisSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
